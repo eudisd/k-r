61a4e2c2e6aff7e5eaeda16d22267520b17727ab
Eudis Duran <eudisd@live.com>
2011-05-18 23:26:59 -0400

    Wrote the problem description.

--- chapter-1/1-20.c
+++ chapter-1/1-20.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/*  Problem:
+ *  Write a program that replaces tabs in the input with
+ *  the proper number of blanks to space to the next tab
+ *  stop.  Assume a fixed set of tab stops, say every n
+ *  columns.  Should n be a variable or symbolic parameter
+ */
+
 int main()
 {
     return 0;
