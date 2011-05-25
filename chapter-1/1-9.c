e4dc7ca56fd523dc87fa471ad42651aade950bde
Eudis Duran <eudisd@live.com>
2011-05-24 20:02:19 -0400

    Finished problem 1-9.c.

---
+++ chapter-1/1-9.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* Problem: 
+ * Write a program to copy its input to its 
+ * output, replacing each string of one or more
+ * blanks by a single blank
+ */
+
+#define BUF_SIZE 1024
+
+int main()
+{
+    char input[BUF_SIZE] = "\nSample input      here";
+    int i;
+
+    for(i = 0; i < BUF_SIZE; i++){
+        if( input[i] == ' ' ){
+            while( input[i] == ' ' ){
+                i++;
+            }
+            printf(" %c", input[i]);
+        }
+        else {
+            printf("%c", input[i]);
+        }
+    }
+}
