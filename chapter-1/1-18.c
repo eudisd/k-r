ac3d69fdc04ae37349ab4cd73110eec78bb14770
Eudis Duran <eudisd@live.com>
2011-09-21 11:03:14 -0400

    Started exercise 1-18.c

---
+++ chapter-1/1-18.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define MAX_INPUT 80
+
+char *remove_blanks(char *line);
+
+int main(int argc, char **argv){
+
+    char *line = (char*)malloc(sizeof(char)*MAX_INPUT);
+    int max = MAX_INPUT; 
+    
+    while( *line != EOF ){
+        getline(&line, &max, stdin);
+    }
+    printf("Old Line Length: %d\n", (size_t)strlen(line));
+    line = remove_blanks(line);
+    printf("New Line Length: %d\n", (size_t)strlen(line));
+    printf("Line: %s\n", line);
+
+    return EXIT_SUCCESS;
+}
+
+char *remove_blanks(char *line){
+    return NULL;
+}
