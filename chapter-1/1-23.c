0594e7a99c55228d7208c540c001301f153f1ba2
Eudis Duran <eudisd@live.com>
2011-09-26 21:46:14 -0400

    Added 1-23

---
+++ chapter-1/1-23.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* Problem: Write a program to remove all comments from a C program.  Don't
+ * forget to handle quoted strings and character constants properly.  C
+ * comments do not nest.
+ */
+
+int main(){
+
+    return EXIT_SUCCESS;
+}
