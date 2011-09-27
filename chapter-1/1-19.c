007a590eda5d064868968663b0a21015fcedfb5f
Eudis Duran <eudisd@live.com>
2011-09-26 21:10:16 -0400

    Started exercise 1-19

---
+++ chapter-1/1-19.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void reverse(char **s);
+
+int main(){
+    
+    char *line = "Here goes";
+    
+    printf("%s\n", line);
+    reverse(line);
+    printf("%s\n", line);
+    return EXIT_SUCCESS;
+}
+
+void reverse(char **s){
+    
+}
