c61a946258ce4b3f5a45aea5b2f33088d7cb3bab
Eudis Duran <eudisd@live.com>
2011-09-26 21:44:45 -0400

    Added 1-22

--- chapter-1/1-21.c
+++ chapter-1/1-21.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/* Write a program detab that replaces tabs in the input with the proper
+ * number of blanks to space to the next tab stop.  Assume a fixed set of
+ * tab stops, say every n columns.  Should n be a variable or asymbolic
+ * parameter? 
+ */
+
 int main(){
     return EXIT_SUCCESS;
 }
---
+++ chapter-1/1-22.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+/* Problem: Write a program to "fold" long input lines into two or more
+ * shorter lines after the last non-blank character that occurs before the
+ * n-th column of input.  Make sure your program does something intelligent
+ * with very long lines, and if there are no blanks or tabs before the
+ * specified column. 
+ */
+int main(){
+    return EXIT_SUCCESS;
+}
