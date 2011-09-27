ce68fda850a57deee051855af41cfd3d366eee75
Eudis Duran <eudisd@live.com>
2011-09-26 21:48:24 -0400

    Added 1-24.c

---
+++ chapter-1/1-24.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+/* Problem: Write a program to check a C program for rudimentary syntax
+ * errors like unbalanced parentheses, brackets and braces.  Don't forget
+ * about quotes, both single and double, escape sequences, and comments.
+ * (This program is hard if you do it in generality).
+ */
+
+int main(){
+    return EXIT_SUCCESS;
+}
