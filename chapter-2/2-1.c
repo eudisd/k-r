0757f98ff736c46f05150642e5d2f727e78299d3
Eudis Duran <eudisd@live.com>
2011-10-30 13:52:11 -0400

    Signed non computed

--- chapter-2/2-1.c
+++ chapter-2/2-1.c
@@ -1,14 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <limits.h>
+#include <float.h>
 
 int main(void){
     
     printf("Signed\n\n");
-    printf("char: %d\n"
-           "short: %d\n"
-           "int: %d\n"
-           "long: %d\n",
-            sizeof(char), sizeof(short), sizeof(int), sizeof(long)); 
+    printf("char - Range [%d, %d]\n"
+           "short - Range [%d, %d] %d\n"
+           "int - Range [%d, %d]\n"
+           "long - Range [%d, %d]\n",
+            CHAR_MIN, CHAR_MAX,
+            SHRT_MIN, SHRT_MAX,
+            INT_MIN, INT_MAX,
+            LONG_MIN, LONG_MAX); 
     
     printf("\n\nUnsigned\n\n");
     
@@ -16,8 +21,10 @@ int main(void){
            "unsigned short: %d\n"
            "unsigned int: %d\n"
            "unsigned long: %d\n",
-            sizeof(unsigned char), 
+             
             sizeof(unsigned short), 
             sizeof(unsigned int), 
-            sizeof(unsigned long)); 
+            sizeof(unsigned long));
+
+
     }
