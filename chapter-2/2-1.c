e3c39a797bd2471f00b75a12f125d30e03911289
Eudis Duran <eudisd@live.com>
2011-10-30 13:32:43 -0400

    Finished exercise 2-1.c

---
+++ chapter-2/2-1.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void){
+    
+    printf("char: %d, short: %d, int: %d, long: %d\n",
+            sizeof(char), sizeof(short), sizeof(int), sizeof(long));
+}
