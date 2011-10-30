00d733ae3ffc14e89d7d28d0df39791331d69ffb
Eudis Duran <eudisd@live.com>
2011-10-30 13:38:05 -0400

    Actually, not doneyet with 2-1 :D, still working on it

--- chapter-2/2-1.c
+++ chapter-2/2-1.c
@@ -3,6 +3,21 @@
 
 int main(void){
     
-    printf("char: %d, short: %d, int: %d, long: %d\n",
-            sizeof(char), sizeof(short), sizeof(int), sizeof(long));
-}
+    printf("Signed\n\n");
+    printf("char: %d\n"
+           "short: %d\n"
+           "int: %d\n"
+           "long: %d\n",
+            sizeof(char), sizeof(short), sizeof(int), sizeof(long)); 
+    
+    printf("\n\nUnsigned\n\n");
+    
+    printf("unsigned char: %d\n"
+           "unsigned short: %d\n"
+           "unsigned int: %d\n"
+           "unsigned long: %d\n",
+            sizeof(unsigned char), 
+            sizeof(unsigned short), 
+            sizeof(unsigned int), 
+            sizeof(unsigned long)); 
+    }
