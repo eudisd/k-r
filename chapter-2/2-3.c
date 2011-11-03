65fb6322bfd88c82a8d86e26f8b6dde105492ab5
Eudis Duran <eudisd@live.com>
2011-11-03 00:47:15 -0400

    Wrote the skeleton for problem 2-3.c

---
+++ chapter-2/2-3.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/** Exercise: Write the function htoi(s), which converts a string
+ * of hexidecimal digits (including an optional 0x or 0X) into it's 
+ * equivalent integer value.  The allowable digits are 0 through 9,
+ * a through f, and A through F.
+ **/
+
+int main(void){
+    return 0;
+}
