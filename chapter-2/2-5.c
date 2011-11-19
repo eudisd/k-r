c6ecdf5d4e7c52896069ce30bd05fac9a03571fb
Eudis Duran <eudisd@gmail.com>
2011-11-18 23:37:11 -0500

    Started work on example 2-5

---
+++ chapter-2/2-5.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+/* Problem:
+ * Write the function any(s1, s2) which returns the first location in the
+ * string s1 where any character from the string s2 occurs, or -1 if s1
+ * contains no characters from s2. 
+ */
+int main(void){
+
+    return 0;
+}
