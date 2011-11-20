9924d218e1f590592f653bb29b5b4f9811788a7c
Eudis Duran <eudisd@live.com>
2011-11-20 18:58:09 -0500

    Added skeleton for 2-6.c

---
+++ chapter-2/2-6.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* Problem:
+ * Write a function setbits(x, p, n, y) that returns x wht the n bits that
+ * begin at position p set to the rightmost n bits of y, leaving the other
+ * bits unchanged. 
+ */
+
+int main(void){
+
+}
