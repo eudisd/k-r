be6e412599d41c5484dd28e6abf004b92056df24
Eudis Duran <eudisd@live.com>
2011-11-20 19:02:58 -0500

    Add 2-8.c template

---
+++ chapter-2/2-8.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* Problem:
+ * Write a function rightrot(x, y) that returns the value of the interger x
+ * rotated ot the right by n bit positions.
+ */
+
+int main(void){
+    return EXIT_SUCCESS;
+}
