07537f815641ad4c1900c05d96be58e3d5d63764
Eudis Duran <eudisd@live.com>
2011-11-20 19:08:00 -0500

    Added 2-10.c problem template

---
+++ chapter-2/2-10.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* Problem: Rewrite the function lower, which convert upper case letters to
+ * lover case, with a conditional expression instead of if-else.
+ */
+
+int main(void){
+    return EXIT_SUCCESS;
+}
