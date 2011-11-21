14435da4438b9d192db41a3b9dcb7d2154d8482e
Eudis Duran <eudisd@live.com>
2011-11-20 19:00:49 -0500

    Added template for problem 2-7.c

---
+++ chapter-2/2-7.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* Problem:
+ * Write a function invert(x, p, n) that returns x with n bits that begin
+ * at position p inverted (i.e., 1 changed into 0 and vice versa), leaving
+ * the others unchanged.
+ */
+
+int main(void){
+    return EXIT_SUCCESS;
+}
