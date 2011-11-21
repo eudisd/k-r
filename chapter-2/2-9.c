a8d4347c52f631b578a86c2cf82b25b5f56bf4fa
Eudis Duran <eudisd@live.com>
2011-11-20 19:05:47 -0500

    Added 2-9.c problem template

---
+++ chapter-2/2-9.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* Problem:
+ * In a two's complement number system, x &= (x - 1) deleted the rightmost
+ * 1-bit in x.  Explain why.  Use this observation to write a faster
+ * version of bitcount
+ */
+
+int main(void){
+    return EXIT_SUCCESS;
+}
