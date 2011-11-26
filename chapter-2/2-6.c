b24ba700e6860354def4c2b83b0cbd2c91b39b71
Eudis Duran <eudisd@gmail.com>
2011-11-26 12:01:38 -0500

    Working on 2-6.  Wrote a hleper function to print bit fields

--- chapter-2/2-6.c
+++ chapter-2/2-6.c
@@ -2,11 +2,29 @@
 #include <stdlib.h>
 
 /* Problem:
- * Write a function setbits(x, p, n, y) that returns x wht the n bits that
+ * Write a function setbits(x, p, n, y) that returns x with the n bits that
  * begin at position p set to the rightmost n bits of y, leaving the other
  * bits unchanged. 
  */
 
+void printbits(int x);
+void setbits(int x, int p, int n, int y);
+
 int main(void){
+    int x = 2;
+    printbits(x);
+}
+
+void printbits(int x){
+    int i;
+    printf("int x = %d\n\n", x);
+    for(i = sizeof(int)*8 - 1; i > 0; i--){
+        printf("%d", (x >> i) & 0x1); 
+    }
+    printf("%d", x & 0x1);
+}
+
+void setbits(int x, int p, int n, int y){
+
 
 }
