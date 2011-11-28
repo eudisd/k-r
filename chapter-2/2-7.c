05f4f6fddaaf1a2c93a33d0122c04c8d25243b81
Eudis Duran <eudisd@gmail.com>
2011-11-27 20:53:43 -0500

    Added the helper functions from 2-6 to 2-7

--- chapter-2/2-7.c
+++ chapter-2/2-7.c
@@ -7,6 +7,17 @@
  * the others unchanged.
  */
 
+void printbits(char s[32], int x);
+
 int main(void){
     return EXIT_SUCCESS;
+}
+
+void printbits(char s[32], int x){
+    int i;
+    printf("\nint %s = %d\n\n", s, x);
+    for(i = sizeof(int)*8 - 1; i > 0; i--){
+        printf("%d", (x >> i) & 0x1); 
+    }
+    printf("%d\n", x & 0x1);
 }
