ea7002d4b3b9765a19579e43beddc908c79e0e46
Eudis Duran <eudisd@gmail.com>
2011-11-29 22:57:28 -0500

    Started working on chapter 3

---
+++ chapter-3/3-2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/* Problem: write a version of the binary search given
+ * with only one test inside the loop and measure the
+ * difference in run-time
+ */
+int main(void)
+{
+    return EXIT_SUCCESS;
+}
