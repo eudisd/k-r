504bfe72f2b18361e6a2e4b525950b6ae9ddacc2
Eudis Duran <eudisd@gmail.com>
2011-11-29 22:59:51 -0500

    Wrote the skeleton for 3.1 and also the vinalla binsearch in questions

--- chapter-3/3-2.c
+++ chapter-3/3-2.c
@@ -6,7 +6,28 @@
  * with only one test inside the loop and measure the
  * difference in run-time
  */
+
+int binsearch(int x, int v[], int n);
+
 int main(void)
 {
     return EXIT_SUCCESS;
+}
+
+int binsearch(int x, int v[], int n)
+{
+    int low, high, mid;
+    low = 0;
+    high = n - 1;
+    while(low <= high){
+        mid = (low + high)/2;
+        if(x < v[mid])
+            high = mid - 1;
+        else if(x > v[mid])
+            low = mid + 1;
+        else
+            return mid;
+    }
+
+    return -1;
 }
