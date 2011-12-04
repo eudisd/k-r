d2d29cd29990f5dbde87c128f25d10ed099e672e
Eudis Duran <eudisd@gmail.com>
2011-12-04 00:48:20 -0500

    Working on the the timing of the execution time for binsearch

--- chapter-3/3-1.c
+++ chapter-3/3-1.c
@@ -1,19 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <sys/time.h>
 
 /* Problem: write a version of the binary search given
  * with only one test inside the loop and measure the
  * difference in run-time
  */
 
+double microsec(void);
 int binsearch(int x, int v[], int n);
 
 int main(void)
 {
+    int i;
+    double t1, t2, diff=0;
+    int v[] = {1,2,3,4,5,6,7,8,9,10};
+    for(i = 0; i < 30; i++){
+        t1 = microsec();
+        binsearch(1, v, 10);
+        t2 = microsec();
+        diff += (t2.tv_usec - t1.tv_usec);
+    }
+    diff /= 30.0;
+    printf("No optimization: %f\n", diff);
     return EXIT_SUCCESS;
 }
-
+double microsec(void)
+{
+    struct timeval t1;
+    struct timezone tz;
+    return (double)gettimeofday(&t1, &tz);
+}
 int binsearch(int x, int v[], int n)
 {
     int low, high, mid;
