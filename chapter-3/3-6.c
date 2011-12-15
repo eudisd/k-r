13e544fd2eafeb8b10bb54e89f81fc20fe35096b
Eudis Duran <eudisd@live.com>
2011-12-15 15:44:41 -0500

    ...

---
+++ chapter-3/3-6.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* Write a version of itoa that accepts three argument instead of two.  The
+ * third argument is a minimum field width; the converted number must be
+ * padded with blanks on the left if necessary to make it wide enough.
+ */
+int main(void)
+{
+    return 0;
+}
