0f3cbf0659ebc0d4715798b918345297b5cdaa60
Eudis Duran <eudisd@live.com>
2012-01-09 07:36:51 -0500

    Added reverse to the main file, and also itoa template

--- chapter-3/3-4.c
+++ chapter-3/3-4.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 /* In a two's complement number representation, our version of itoa does
  * not handle the largest negative number, that is, the value of n equals
@@ -13,8 +14,38 @@
  * since the largest negative number cannot be stored as the largest
  * postive number in a word (int).
  */
+void reverse(char *s);
+void itoa(int n, char s[]);
 
 int main(void)
 {
+    char ar[100];
+    itoa(20, ar);
+    printf("%s", ar);
     return 0;
+}
+void reverse(char *s){
+    int tmp, i, len = strlen(s);
+    for(i = 0; i < len/2; i++){
+        tmp = s[i];
+        s[i] = s[(len - 1) - i];
+        s[(len - 1) - i] = tmp;  
+    }
+}
+       
+void itoa(int n, char s[]){
+    int i, sign;
+    if((sign = n) < 0)
+        n = -n;
+
+    i = 0;
+
+    do {
+        s[i++] = n % 10 + '0';
+    } while((n /= 10) > 0);
+    if(sign > 0){
+        s[i++] = '-';
+    }
+    s[i] = '\0';
+    reverse(s);
 }
