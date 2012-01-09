86fe3643f7efd2549a43287b42568ee25278af6d
Eudis Duran <eudisd@live.com>
2012-01-09 09:16:29 -0500

    Progress on 3-3

--- chapter-3/3-3.c
+++ chapter-3/3-3.c
@@ -13,7 +13,7 @@ void expand(char *s1, char *s2);
 
 int main(void)
 {
-    char *s1 = "a-z", s2[100];
+    char *s1 = "-a-z", s2[100] = {0};
     printf("s1: %s\n", s1);
     expand(s1, s2);
     printf("\nExpanded: %s\n", s2);
@@ -23,10 +23,47 @@ int main(void)
 void expand(char *s1, char *s2)
 {
     int l = strlen(s1);
+    int start_flag = 0, end_flag = 0, shift = 0;
+    int a, b, c, i;
+    
     if(s1[0] == '-'){
         s2[0] = '-';
+        start_flag = 1;
     } 
+    /* a-b-c */
+
+    if(start_flag == 0){
+        shift = 0;
+    } else {
+        shift = 1;
+    }
+    /* First, test what it is */
+    if( l == 3 || l == 4 ){ /* a - z */
+        for(i = shift; i <= (s1[l - 1] - s1[shift]); i++){
+            if(shift == 0)
+                s2[i] = s1[shift] + i;
+            else 
+                s2[i] = s1[shift] + (i - 1);
+        }
+    } else if(s1[shift + 1] == '-' && s1[shift + 3] == '-'){  /* a-b-c */
+        a = s1[shift];
+        b = s1[shift + 2];
+        c = s1[shift + 4];
+        for(i = shift; i < (b - a); i++){ /* a - b */
+            if(shift == 0)
+                s2[i] = a + i;
+            else 
+                s2[i] = a + (i - 1);
+        }
+        for(; i <= (b - a) + (c - b); i++){
+            if(shift == 0)
+                s2[i] = a + i;
+            else 
+                s2[i] = a + (i - 1);
+        }
+    } else if(s1[shift + 1] == '-' && s1[shift + 4] == '-'){ /* a-z0-9 */
     
+    }
     
     if(s1[l - 1] == '-'){
     
