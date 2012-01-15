0c26327cfc7d6da3d3a52c0626779a8cd8a4c2c1
Eudis Duran <eudisd@live.com>
2012-01-15 12:48:19 -0500

    Now finished with 3-6

--- chapter-3/3-6.c
+++ chapter-3/3-6.c
@@ -10,13 +10,13 @@
 
 void itoa(double n, char s[], int w);
 void reverse(char *s);
-void shift_right(char *s, int n);
+void shift_right(char *s, int n, int at);
 
 int main(void)
 {
     int integer = 1234;
     char tmp[100];
-    itoa(integer, tmp, 2);
+    itoa(integer, tmp, 1);
     printf("%s\n", tmp); 
     return 0;
 }
@@ -54,12 +54,22 @@ void itoa(double n, char s[], int w){
     if(i > w){
         s[w] = '\0';
     } else if( (w - i) > 0 ) {
-        shift_right(s, (w - i));
+        shift_right(s, (w - i), i);
     }
 
 }
-void shift_right(char *s, int n)
+void shift_right(char *s, int n, int at)
 {
-        
+    int i, j;
+    int tmp = at;
+    for(i = 0; i < n; i++, tmp++){ /* Shift, this many times */
+        for(j = tmp; j > 0; j--) { /* actual shift */
+            s[j] = s[j - 1];
+        }
+    }
+    s[tmp] = '\0';
 
+    for(i = 0; i < n; i++){
+        s[i] = '0';
+    }
 }
