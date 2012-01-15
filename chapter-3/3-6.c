dd8f8b7a2cc47aa3ed35afbc6a6ca7409d61a1d7
Eudis Duran <eudisd@live.com>
2012-01-15 12:08:17 -0500

    Finished 3-6

--- chapter-3/3-6.c
+++ chapter-3/3-6.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 /* Write a version of itoa that accepts three argument instead of two.  The
  * third argument is a minimum field width; the converted number must be
@@ -8,12 +9,25 @@
 
 
 void itoa(double n, char s[], int w);
+void reverse(char *s);
 
 int main(void)
 {
+    int integer = 1234;
+    char tmp[100];
+    itoa(integer, tmp, 2);
+    printf("%s\n", tmp); 
     return 0;
 }
 
+void reverse(char *s){
+    int tmp, i, len = strlen(s);
+    for(i = 0; i < len/2; i++){
+        tmp = s[i];
+        s[i] = s[(len - 1) - i];
+        s[(len - 1) - i] = tmp;  
+    }
+}
 
 void itoa(double n, char s[], int w){
     int i, sign;
@@ -32,6 +46,17 @@ void itoa(double n, char s[], int w){
     if(sign < 0){
         s[i++] = '-';
     }
+
     s[i] = '\0';
     reverse(s);
+    
+    if(i > w){
+        s[w] = '\0';
+    } else if( (w - i) > 0 ) {
+        for(; i < w; i++){
+            s[i] = '0';
+        }
+        s[i] = '\0';
+    }
+
 }
