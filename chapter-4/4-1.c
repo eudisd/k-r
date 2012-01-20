c3627402704e7b506cae8693b1c8f90b9a28d14e
Eudis Duran <eudisd@live.com>
2012-01-20 10:34:31 -0500

    Finished 4-1

--- chapter-4/4-1.c
+++ chapter-4/4-1.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 /* Write the function strrindex(s, t) which returns the position of the
  * rightmost occurrence of t in s, or -1 if there is none.
@@ -9,10 +10,25 @@ int strrindex(char *s, char *t);
 
 int main(void)
 {
+    char *a = "Hello, World, World",
+         *b = "World";
 
+    printf("Right Most Position: %d\n", strrindex(a, b));
 }
 
 int strrindex(char *s, char *t)
 {
-
+    int i, j, k;
+    int len_s = strlen(s);
+    int len_t = strlen(t);
+    for(i = len_s; i > 0; i--){
+        if( (len_s - i) >= len_t ){
+            for(j = i, k = 0; s[j] == t[k] && (t[k] != '\0'); j++, k++)
+                ;
+            if(k > 0 && t[k] == '\0'){
+                return i;
+            }
+        }
+    }
+    return -1;
 }
