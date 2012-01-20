984d2bcac2c9e2f7b602e16cbb2ac179f83d0831
Eudis Duran <eudisd@live.com>
2012-01-20 10:07:30 -0500

    Started on 4-1

---
+++ chapter-4/4-1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* Write the function strrindex(s, t) which returns the position of the
+ * rightmost occurrence of t in s, or -1 if there is none.
+ */
+
+int strrindex(char *s, char *t);
+
+int main(void)
+{
+
+}
+
+int strrindex(char *s, char *t)
+{
+
+}
