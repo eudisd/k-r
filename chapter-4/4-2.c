488790433d8063bd412472c715dadef215164c96
Eudis Duran <eudisd@live.com>
2012-01-29 15:40:44 -0500

    Started work on 4-2.c

--- chapter-4/4-2.c
+++ chapter-4/4-2.c
@@ -17,8 +17,8 @@ int main(void)
 
 double atof(char s[])
 {
-    double val, power;
-    int i, sign;
+    double val, e_val = 1.0, power;
+    int i, sign, e_sign = -2;
 
     for(i = 0; isspace(s[i]); i++)
         ;
@@ -40,5 +40,27 @@ double atof(char s[])
         val = 10.0 * val + (s[i] - '0');
         power *= 10.0;
     }
-    return sign * val / power;
+
+    if(s[i] == 'e' || s[i] == 'E')
+        i++;
+
+    if(s[i] == '-'){
+        e_sign = -1;
+        i++;
+    } else if (s[i] == '+'){
+        e_sign = 1;
+        i++;
+    } 
+    
+    for(e_val = 0.0; isdigit(s[i]); i++){
+        e_val = 10.0*e_val + (s[i] - '0');
+    }
+
+    if(e_sign == -2){
+        return sign * val / power;
+    } else if (e_sign == -1){
+
+    } else if (e_sign == 1){
+
+    }
 }
