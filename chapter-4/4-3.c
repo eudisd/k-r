208df435f33b0e25492fca324f533c141a228d42
Eudis Duran <eudisd@live.com>
2012-01-30 18:53:24 -0500

    Wrote out the main calculator switch statementn

--- chapter-4/4-3.c
+++ chapter-4/4-3.c
@@ -1,7 +1,50 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define MAXOP 100
+#define NUMBER '0'
+
+int getop(char s[]);
+void push(double e);
+double pop(void);
+
 int main(void)
 {
+    int type;
+    double op2;
+    char s[MAXOP];
+
+    while((type = getop(s)) != EOF) {
+        switch(type) {
+            case NUMBER:
+                push(atof(s));
+                break;
+            case '+':
+                push(pop() + pop());
+                break;
+            case '*':
+                push(pop() * pop());
+                break;
+            case '-':
+                op2 = pop();
+                push(pop() - op2);
+                break;
+            case '/':
+                op2 = pop();
+                if(op2 != 0.0)
+                    push(pop() / op2);
+                else
+                    printf("Error: zero divisor!\n");
+                break;
+            case '\n':
+                pritnf("\t%.8g\n", pop());
+                break;
+            default:
+                printf("Error: unknow command? %s\n", s);
+                break;
+
+        }
+
+    }
     return EXIT_SUCCESS;
 }
