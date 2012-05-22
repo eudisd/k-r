64c73267f6a97fa894ea23f287953afb530addaf
Eudis Duran <eudisd@gmail.com>
2012-05-22 16:53:58 -0400

    Working out some simple test...

--- chapter-4/4-12.c
+++ chapter-4/4-12.c
@@ -1,13 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/* Exercise - 4-12: 
- * Adapt the ideas 'printd' to write a recursive version of itoa;  that is
- * convert an integer into a string by calling a recursive routine.
- */
+/** Exercise - 4-12: 
+* Adapt the ideas 'printd' to write a recursive version of itoa;  that is
+* convert an integer into a string by calling a recursive routine.
+**/
 
+char *itoa(int n);
+ 
 int main(void)
 {
+    int n = 20;
+    printf("Integer: %d\n", n);
+    printf("Alpha: %s\n", itoa(n));
     
     return EXIT_SUCCESS;
+}
+
+char *itoa(int n)
+{
+    return NULL;
 }
