c9270f721c8d41245b26ae2d2e349f68f5d0b4ab
Eudis Duran <eudisd@gmail.com>
2012-05-22 16:50:11 -0400

    Skipped a bunch of exercises dealing with the calculator.  Will do that later. For now, working on exercise 4-12.c

---
+++ chapter-4/4-12.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* Exercise - 4-12: 
+ * Adapt the ideas 'printd' to write a recursive version of itoa;  that is
+ * convert an integer into a string by calling a recursive routine.
+ */
+
+int main(void)
+{
+    
+    return EXIT_SUCCESS;
+}
