56ee8a26a2b741616c3d791e8f991153d3e3b1f8
Eudis Duran <eudisd@gmail.com>
2012-05-22 17:48:56 -0400

    4-12 complete

--- chapter-4/4-12.c
+++ chapter-4/4-12.c
@@ -12,7 +12,7 @@ char *itoa(int n, char s[]);
  
 int main(void)
 {
-    int n = 20;
+    int n = -2000;
     char buffer[MAX_BUFFER_SIZE];
     
     printf("Integer: %d\n", n);
@@ -23,21 +23,35 @@ int main(void)
 
 char *itoa(int n, char s[])
 {
-    static int i = 0;
+    static int i = 0, j = 0;
     
     if (i >= MAX_BUFFER_SIZE){
         printf("\nBuffer too small to hold string representation!  Exiting...\n\n");
         exit(EXIT_FAILURE);
     }
     
-    if (n < 0) {
+    if ( n < 0 ) {
+        s[i++] = '-';
         n = -n;
     }
-    if (n / 10) {
-        itoa(n/10, s);
-        s[i++] = '0' + n % 10;
-    } else {
-        s[i] = '\0';
+    
+    if ( n / 10 >= 0 ) {
+        if ( n / 10 ) {
+            j++;
+            s = itoa(n / 10, s);
+        }
+        
+        s[i] = '0' + n % 10;
+        i++;
+        
         return s;
+        
+        if ( n / 10 == 0){
+            s[j] = '\0';
+            return s;
+        }
+
+    } else {
+        return "<error>";
     }
 }
